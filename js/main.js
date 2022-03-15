67ac80639d5a070ed6d299b7598d9e54be30d05a
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-03-16 00:31:29 +0100

    web

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// global variables
+//var xxx = 0;
+
+jQuery(document).ready(function(){ 
+	// http://api.jquery.com/bind/
+	// http://api.jquery.com/css/
+	// http://api.jquery.com/text/
+	//$('p').css({border: '5px solid red', color: 'green', padding: '20px'});
+
+});
+
